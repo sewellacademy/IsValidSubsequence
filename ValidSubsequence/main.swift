686542e7f50565d7617cbd70c637849343644727
dylanswl <dvsewell15@gmail.com>
2022-01-18 09:02:30 -0500

    isValidSubsequence solutions with while loop and for loop approaches

--- ValidSubsequence/main.swift
+++ ValidSubsequence/main.swift
@@ -7,5 +7,39 @@
 
 import Foundation
 
-print("Hello, World!")
+class ProgramSubsequence {
+    // O(n) time | O(1) space
+    static func isValidSubsequenceWhileLoop(_ array: [Int], _ sequence: [Int]) -> Bool {
+        var arrIdx = 0
+        var seqIdx = 0
+        
+        while arrIdx < array.count, seqIdx < sequence.count {
+            if array[arrIdx] == sequence[seqIdx] {
+                seqIdx += 1
+            }
+            arrIdx += 1
+        }
+        return seqIdx == sequence.count
+    }
+    
+    // O(n) time | O(1) space
+    static func isValidSubsequenceForLoop(_ array: [Int], _ sequence: [Int]) -> Bool {
+        var seqIdx = 0
+        for value in array {
+            if seqIdx == sequence.count {
+                break
+            }
+            if value == sequence[seqIdx] {
+                seqIdx += 1
+            }
+        }
+        return seqIdx == sequence.count
+    }
+}
+
+let array: [Int] = [5, 1, 22, 25, 6, -1, 8, 10]
+let sequence: [Int] = [1, 6, -1, 10]
+
+print(ProgramSubsequence.isValidSubsequenceWhileLoop(array, sequence))
+print(ProgramSubsequence.isValidSubsequenceForLoop(array, sequence))
 
